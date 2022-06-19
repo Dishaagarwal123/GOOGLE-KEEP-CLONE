e402014d5b1f18f2bd123a989e404f2909e9fafc
disha agarwal <89624470+disha-rani@users.noreply.github.com>
2022-06-19 16:48:58 +0530

    Update script.js

--- GOOGLE-KEEP_CLONE/script.js
+++ GOOGLE-KEEP_CLONE/script.js
@@ -1,40 +1,90 @@
-class notes_class{
-    constructor(title,note){
-       this.title=title
-        this.note=note
-    }
-}
-class display{
-    constructor(notes){
-        this.notes=notes
-    }
-    addnote(){
-        const ul=document.getElementById('ul')
-        const data=document.createElement('li')
-        data.innerHTML=`<div class="card" style="width: 18rem;">
-        <div class="card-body">
-          <h5 class="card-title">${this.notes.title}</h5>
-          <p class="card-text">${this.notes.note}</p>
-          <a href="#" class="btn btn-primary">delete</a>
-        </div>
-      </div>`
-      ul.append(data)
-      console.log(data);
-    }
-}
-const form=document.getElementById('form'); 
+ 
+console.log("Welcome to notes app. This is app.js");
+showNotes();
 
-form.addEventListener('submit',notesubmit);
+// If user adds a note, add it to the localStorage
+let addBtn = document.getElementById("addBtn");
+addBtn.addEventListener("click", function(e) {
+  let addTxt = document.getElementById("addTxt");
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  notesObj.push(addTxt.value);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  addTxt.value = "";
+//   console.log(notesObj);
+  showNotes();
+});
 
-
-function notesubmit(e){
-    e.preventDefault();
-    console.log('added');
-    const title=document.getElementById('title').value;
-    const note=document.getElementById('note').value;
-    const notes=new notes_class(title,note);
-    console.log(notes);
-    const displaynote=new display(notes);
-    // console.log(displaynote.addnote);
-    displaynote.addnote(notes);
+// Function to show elements from localStorage
+function showNotes() {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  let html = "";
+  notesObj.forEach(function(element, index) {
+    html += `
+            <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Note ${index + 1}</h5>
+                        <p class="card-text"> ${element}</p>
+                        <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
+                    </div>
+                </div>`;
+  });
+  let notesElm = document.getElementById("notes");
+  if (notesObj.length != 0) {
+    notesElm.innerHTML = html;
+  } else {
+    notesElm.innerHTML = `<br><br>Notes you add appear here !!`;
+  }
 }
+
+// Function to delete a note
+function deleteNote(index) {
+//   console.log("I am deleting", index);
+
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+
+  notesObj.splice(index, 1);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  showNotes();
+}
+
+
+let search = document.getElementById('searchTxt');
+search.addEventListener("input", function(){
+
+    let inputVal = search.value.toLowerCase();
+    // console.log('Input event fired!', inputVal);
+    let noteCards = document.getElementsByClassName('noteCard');
+    Array.from(noteCards).forEach(function(element){
+        let cardTxt = element.getElementsByTagName("p")[0].innerText;
+        if(cardTxt.includes(inputVal)){
+            element.style.display = "block";
+        }
+        else{
+            element.style.display = "none";
+        }
+        // console.log(cardTxt);
+    })
+})
+
+/*
+Further Features:
+1. Add Title
+2. Mark a note as Important
+3. Separate notes by user
+4. Sync and host to web server 
+*/ 
